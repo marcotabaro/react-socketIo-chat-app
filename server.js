5d221b1e78cd7d6ebffa225469b5fc30811ccf93
marco <g.marcotaba@gmail.com>
2021-05-03 11:24:23 +0200

    connection between users connected on the same server

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const io = require('socket.io')(3000)
+const io = require('socket.io')(3000, {
+    cors:{origin:"*",},
+});
 
 io.on('connection', socket => {
-    socket.emit('chat-message', 'Hello World')
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', message) //Send the message to everyone else connected to the server, except for u sent the msg
+    });
 });
