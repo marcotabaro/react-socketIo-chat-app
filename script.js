589f788c5e7043ada1fa9739c06f54eee3268d2e
marco <g.marcotaba@gmail.com>
2021-05-03 11:38:56 +0200

    added user names with prompt command

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const socket = io('http://localhost:3000');
 
+const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 
 
+const myName = prompt("What is your name?");
+
+appendMessage('You joined');
+socket.emit('new-user', myName);
+
 socket.on('chat-message', data => {
-    console.log(data);
-})
+    appendMessage(data);
+});
+
+socket.on('user-connected', myName => {
+    appendMessage(`${myName} connected`);
+});
+
+
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault();
     const message = messageInput.value;
     socket.emit('send-chat-message', message)
     messageInput.value = '';
-})
+});
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+};
--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const io = require('socket.io')(3000, {
     cors:{origin:"*",},
 });
 
+const users = {};
+
 io.on('connection', socket => {
+    socket.on('new-user', myName => {
+        users[socket.id] = myName;
+        socket.broadcast.emit('user-connected', myName);
+    })
     socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', message) //Send the message to everyone else connected to the server, except for u sent the msg
+        socket.broadcast.emit('chat-message', {message: message, myName: users[socket.id]}) //Send the message to everyone else connected to the server, except for who sent the msg
     });
 });
