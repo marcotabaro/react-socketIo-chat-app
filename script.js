a9a57e30f3fcdb58de9f805fc880ecff304183de
marco <g.marcotaba@gmail.com>
2021-05-03 11:43:26 +0200

    showing the sent messages

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ socket.on('user-connected', myName => {
 messageForm.addEventListener('submit', e => {
     e.preventDefault();
     const message = messageInput.value;
+    appendMessage(`You: ${message}`);
     socket.emit('send-chat-message', message)
     messageInput.value = '';
 });
